aade9783badfec14c2fd296c54194c71802b00d3
Julien Le Caro <julienlecaro@hotmail.com>
2021-12-28 09:32:08 +0100

    Copie de fichiers

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+const fs = require("fs")
+const sourceDir = "source"
+const destinationDir = "destination"
+
+fs.readdir(sourceDir, (err, files) => {
+    files.forEach(fileName => {
+        copyFile(sourceDir + "/" + fileName, destinationDir + "/" + fileName);
+    })
+})
+
+function copyFile (source, destination)
+{
+    console.log("Copie de " + source + " vers " + destination)
+    fs.copyFile(source, destination, (err) => {
+        if (err) {
+            throw err
+        }
+        console.log('Fichier copié !')
+    });
+}
