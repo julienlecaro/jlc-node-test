ea36885d480a3ebb64b450b3c3b897f1a4be366d
Julien Le Caro <julienlecaro@hotmail.com>
2022-01-05 22:32:15 +0100

    Refactoring

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const fs = require("fs")
-const sourceDir = "source"
-const destinationDir = "destination"
+const fs = require('fs')
+const sourceDir = 'source'
+const destinationDir = 'destination'
 
 fs.readdir(sourceDir, (err, files) => {
-    files.forEach(fileName => {
-        copyFile(sourceDir + "/" + fileName, destinationDir + "/" + fileName);
-    })
+  if (err) {
+    throw err
+  }
+  files.forEach(fileName => {
+    copyFile(sourceDir + '/' + fileName, destinationDir + '/' + fileName)
+  })
 })
 
-function copyFile (source, destination)
-{
-    console.log("Copie de " + source + " vers " + destination)
-    fs.copyFile(source, destination, (err) => {
-        if (err) {
-            throw err
-        }
-        console.log('Fichier copié !')
-    });
+function copyFile (source, destination) {
+  console.log('Copie de ' + source + ' vers ' + destination)
+  fs.copyFile(source, destination, (err) => {
+    if (err) {
+      throw err
+    }
+    console.log('Fichier copié !')
+  })
 }
